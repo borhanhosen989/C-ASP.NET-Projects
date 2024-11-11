1163198407e6581ffcfea699a1b31396342e275d
MD Borhan Hosen <borhanhosen989@gmail.com>
2024-11-11 19:59:34 +0600

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projects
+{
+    internal class Projects
+    {
+
+
+        static void Main(string[] args)
+        {
+           //  Create firs C# code "Hello World"  
+           Console.WriteLine("Hello World");
+           
+        }
+    }
+}
