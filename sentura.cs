3cef4522ab14bf13756f3201fcd8d92c78131dac
MD Borhan Hosen <borhanhosen989@gmail.com>
2024-11-12 20:37:52 +0600

    Addition, Subtraction, Multiplication, Division
    Here is the file and this file included that addition, subtraction, multiplication, division and reminder program of two integer value

---
+++ sentura.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            // Sum of two integer value
+            Console.WriteLine($"Add two integer value 1 + 2 = {1 + 2}");
+
+            // Subtract of two integer value 
+            Console.WriteLine($"Sub two integer value 10 - 5 = {10 - 5}");
+
+            // Multiply of two integer value 
+            Console.WriteLine($"Multiple two integer value 100 * 50 = {100 * 50}");
+
+            // Division of two integer value 
+            Console.WriteLine($"Divide two integer value 80 / 5 = {80 / 5}");
+
+            // Reminder of two integer value 
+            Console.WriteLine($"Reminder two integer value 10 % 3 = {10 % 3}");
+
+
+        }
+    }
+}
