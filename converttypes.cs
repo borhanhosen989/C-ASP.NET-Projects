3d755ac19dafac741db60e09f8e876ec5349790d
MD Borhan Hosen <borhanhosen989@gmail.com>
2024-11-16 08:26:00 +0600

    convert string, int, double and boolean type
    This file program how to convert data type

---
+++ converttypes.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Sentura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int myNumber = 10;
+            double configurationNumber = 5.25;
+            bool isMachineOn = true;
+
+            Console.WriteLine(Convert.ToString(myNumber));    // Convert int to string
+            Console.WriteLine(Convert.ToDouble(myNumber));    // Convert int to double
+            Console.WriteLine(Convert.ToInt32(configurationNumber));  // Convert double to int
+            Console.WriteLine(Convert.ToString(isMachineOn));   // Convert bool to string 
+
+
+
+
+
+        }
+
+
+    }
+    }
+
