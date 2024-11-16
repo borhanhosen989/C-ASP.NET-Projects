c94b507f1724498ac97285bbf7965a26185c1cf1
MD Borhan Hosen <borhanhosen989@gmail.com>
2024-11-16 08:18:20 +0600

    Float types program
    Floating type problem solve in this file

---
+++ floattypes.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Sentura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float f1 = 35e4F;
+            double d1 = 12E4D;
+            Console.WriteLine(f1);
+            Console.WriteLine(d1);
+        }
+
+
+    }
+    }
+
