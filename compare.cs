2d5a44c1a7ec866cbcb730bd64cdc0305ad74559
MD Borhan Hosen <borhanhosen989@gmail.com>
2024-11-17 18:06:24 +0600

    Compare Two Integer Values
    In this file explain and write a code to compare values.

---
+++ compare.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Sentura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Assign Values 
+            int age = 23;
+            int age2 = 24;
+
+            Console.WriteLine(age > age2); 
+
+
+
+
+
+
+        }
+
+
+    }
+    }
+
